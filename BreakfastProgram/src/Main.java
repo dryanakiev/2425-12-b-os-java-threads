02888dd6d21f2ba28d9bffcd645f2bb376bbbf80
dryanakiev <dyanakiev@codingburgas.bg>
2024-11-27 12:34:14 +0200

    Add daemon property example code

--- BreakfastProgram/src/Main.java
+++ BreakfastProgram/src/Main.java
@@ -1,5 +1,27 @@
+class Something extends Thread {
+    public void run() {
+        Thread.currentThread().setName("Something thread");
+
+        while (true){
+            System.out.println("Doing work...");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Thread.currentThread().setName("Main thread");
+
+        Something somethingObject = new Something();
+
+        // Daemon property interrupts something thread
+        somethingObject.setDaemon(true);
+        somethingObject.start();
     }
 }
